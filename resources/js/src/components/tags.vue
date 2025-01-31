2ca460295e8f2b0fbc335f8ad87d2b4baef765cf
kovyakin <kdv-1974@mail.ru>
2025-01-31 16:39:29 +0400

    dev tags 4

--- resources/js/src/components/tags.vue
+++ resources/js/src/components/tags.vue
@@ -5,28 +5,37 @@
       <Card style="width: 20rem; overflow: hidden">
         <template #content>
 
-          <div class="grid grid-cols-12 ">
-            <div >
-              <InputText v-model="title"  :value=" tag.title " type="text"
+              <InputText v-model="title"
+                         class="!w-full"
+                         @value-change="edit_tags(tag)"
+                         :disabled="!tag.disabled" :value=" tag.title " type="text"
                          size="small"  />
+        </template>
+
+        <template #footer>
+          <div class="flex justify-stretch">
+
+                <div class="flex gap-4 mt-1 text-sm mr-4">
+                  created: {{ tag.created_at }}
+                </div>
+            <div class="mx-1 my-1"  v-if="tag.disabled">
+              <i class="pi pi-check" style="color: slateblue"
+                 @click="send_tags(tag)"
+              ></i>
             </div>
-            <div class="col-start-11 flex justify-content-end" >
+
+            <div class="mx-1 my-1" >
               <i class="pi pi-pencil"
+                 @click="edit(tag)"
                  style="color: orange; font-size: 1rem"></i>
             </div>
 
-            <div  class="col-start-12 flex justify-content-end">
+            <div  class="mx-1 my-1">
               <i class="pi pi-times"
                  style="color: red;font-size: 1rem"></i>
             </div>
           </div>
-        </template>
 
-        <template #footer>
-
-          <div class="flex gap-4 mt-1 text-sm">
-            created: {{ tag.created_at }}
-          </div>
         </template>
       </Card>
 
@@ -41,7 +50,7 @@ import {get} from "../../fetch";
 import {onMounted, ref} from "vue";
 import Card from 'primevue/card';
 import InputText from 'primevue/inputtext';
-import Divider from 'primevue/divider';
+
 
 const props = defineProps({
   token: String
@@ -56,8 +65,20 @@ onMounted(() => {
 const load_tags = () => {
   get('/api/tags', props.token, 'GET').then((response) => response.json()).then((result) => {
     tags.value = result.data;
-    console.log(result);
+    // console.log(result);
   });
+}
+
+const edit = (t)=>{
+  t.disabled = true
+}
+const edit_tags = (t)=>{
+
+  console.log(title)
+}
+const send_tags = (t)=>{
+t.disabled=false
+  console.log(title)
 }
 </script>
 
