7fd57dac98e078a291327a2fcc3502338b28d263
kovyakin <kdv-1974@mail.ru>
2025-02-01 21:05:28 +0400

    dev tasks 10

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,6 +20,7 @@
         @endif
     </head>
     <body class="font-sans antialiased dark:bg-black dark:text-white/50">
+
         <div class="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
 
             <div class="relative min-h-screen flex flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white">
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('auth.register');
+    return view('welcome');
 });
 
 Route::middleware([
